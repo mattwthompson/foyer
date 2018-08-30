29ca3c59cb47513ddbb652156f90890d91917425
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2018-08-30 11:07:25 -0500

    Explicitly print out ids of missing dihedral

--- foyer/forcefield.py
+++ foyer/forcefield.py
@@ -406,6 +406,16 @@ class Forcefield(app.ForceField):
 
         proper_dihedrals = [dihedral for dihedral in structure.dihedrals
                             if not dihedral.improper]
+
+        for omm_ids in data.propers:
+            missing_dihedral = True
+            for pmd_proper in structure.rb_torsions:
+                pmd_ids = (pmd_proper.atom1.idx, pmd_proper.atom2.idx, pmd_proper.atom3.idx, pmd_proper.atom4.idx)
+                if pmd_ids == omm_ids:
+                    missing_dihedral = False
+            if missing_dihedral:
+                print('missing improper with ids {}'.format(pmd_ids))
+
         if data.propers and len(data.propers) != \
                 len(proper_dihedrals) + len(structure.rb_torsions):
             msg = ("Parameters have not been assigned to all proper dihedrals. "
